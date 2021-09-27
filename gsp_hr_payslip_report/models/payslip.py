153a9c53f421e5dd60803b49a46634fcafeac54b
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-09-27 13:19:10 +0700

    hra

--- gsp_hr_payslip_report/models/payslip.py
+++ gsp_hr_payslip_report/models/payslip.py
@@ -6,6 +6,8 @@ class payslip(models.Model):
     _inherit = 'hr.payslip'
 
     basic = fields.Float(string="Basic", compute="_get_nett")
+    hra = fields.Float(string="HRA", compute="_get_nett")
+    ca = fields.Float(string="CA", compute="_get_nett")
     nett = fields.Float(string="Nett", compute="_get_nett")
     tunj = fields.Float(string="Tunjangan", compute="_get_nett")
     pot = fields.Float(string="Potogan", compute="_get_nett")
@@ -19,7 +21,9 @@ class payslip(models.Model):
             gross = 0
             tunj = 0
             pot = 0
-            
+            hra = 0
+            ca = 0
+
             for line in rec.line_ids:
                 if line.code == 'BASIC':
                     basic = line.total
@@ -27,8 +31,14 @@ class payslip(models.Model):
                     gross = line.total
                 if line.code == 'NET':
                     nett = line.total
+                if line.code == 'HRA':
+                    hra = line.total
+                if line.code == 'CA':
+                    ca = line.total
 
             rec.tunj = gross - basic
-            rec.pot = nett - gross
+            rec.pot =  gross - nett
             rec.nett = nett
             rec.basic = basic
+            rec.hra = hra
+            rec.ca = ca
