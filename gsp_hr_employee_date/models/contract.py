ff8bcc676303b928a28c3480ab5b5ecf05628517
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-09-27 11:04:30 +0700

    Update contract.py

--- gsp_hr_employee_date/models/contract.py
+++ gsp_hr_employee_date/models/contract.py
@@ -4,4 +4,5 @@ class contract(models.Model):
     _inherit = "hr.contract"
 
     tanggal_aktif = fields.Date(string="Tanggal aktif")
+    employee_id = fields.Many2one(string="Karyawan", comodel_name="hr.employee" )
     
