3f36f53b8d9f40f170a72164655214d09a9e8a5f
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-09-27 13:13:41 +0700

    basic

--- gsp_hr_payslip_report/models/payslip.py
+++ gsp_hr_payslip_report/models/payslip.py
@@ -5,14 +5,30 @@ from odoo import models, fields, api
 class payslip(models.Model):
     _inherit = 'hr.payslip'
 
+    basic = fields.Float(string="Basic", compute="_get_nett")
     nett = fields.Float(string="Nett", compute="_get_nett")
+    tunj = fields.Float(string="Tunjangan", compute="_get_nett")
+    pot = fields.Float(string="Potogan", compute="_get_nett")
 
 
 
     def _get_nett(self):
         for rec in self:
             nett = 0
+            basic = 0
+            gross = 0
+            tunj = 0
+            pot = 0
+            
             for line in rec.line_ids:
+                if line.code == 'BASIC':
+                    basic = line.total
+                if line.code == 'GROSS':
+                    gross = line.total
                 if line.code == 'NET':
                     nett = line.total
+
+            rec.tunj = gross - basic
+            rec.pot = nett - gross
             rec.nett = nett
+            rec.basic = basic
